ed095c6bf06cc33a591131df3af218d41d92c356
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2021-09-03 00:49:58 +0600

    coach registration form added with form-binding

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <section>
+            <base-card>
+                <h2>Coach Rgistration</h2>
+                <coach-form></coach-form>
+            </base-card>
+        </section>
+    </div>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        CoachForm
+    }
+};
+</script>
